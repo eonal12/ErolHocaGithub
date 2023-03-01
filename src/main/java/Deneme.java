68f2d08e6cd9b591780bd88e32a0f0c1ea4e1a96
Emre QA <emreonal12@gmail.com>
2023-03-01 04:30:19 +0300

    v3 benim kodlarım

--- src/main/java/Deneme.java
+++ src/main/java/Deneme.java
@@ -3,5 +3,7 @@ public class Deneme {
     public static void main(String[] args) {
         System.out.println("V1 Çalışmaları");
         System.out.println("V2 Çalışmaları");
+        System.out.println("V3 Çalışmaları");//Bu kodu ben yazdım fakat gitHub'dan pull etmeden yazdığım için
+                                             //bir sonraki versiyon çalışmalarımı push ettiğimde hata alıcam
     }
 }
