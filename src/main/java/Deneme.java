07fdee230466af845470ced131b3991095ec3966
eonal12 <121941963+eonal12@users.noreply.github.com>
2023-03-01 04:28:13 +0300

    v3

--- src/main/java/Deneme.java
+++ src/main/java/Deneme.java
@@ -3,5 +3,6 @@ public class Deneme {
     public static void main(String[] args) {
         System.out.println("V1 Çalışmaları");
         System.out.println("V2 Çalışmaları");
+        System.out.println("V3 Çalışmaları");//Bu kodu başka bir developer yazarak gitHub'a gönderdi
     }
 }
